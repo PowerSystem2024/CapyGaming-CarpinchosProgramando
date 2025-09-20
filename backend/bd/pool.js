8dbad9f50169ba2f7881a03fbc9cbfe1d8560002
Meratim <guerra.mercedes@hotmail.com>
2025-09-20 02:24:32 -0300

    [ADD] Agrego carpeta .env con contraseñas y datos de la base de datos. Agrego mensajes de depuracion para ver por consola

--- backend/bd/pool.js
+++ backend/bd/pool.js
@@ -1,11 +1,22 @@
+<<<<<<< Updated upstream
 const { Pool } = require('pg');
+=======
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+>>>>>>> Stashed changes
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'capygaming',
-    password: 'admin',
-    port: 5432,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
 });
 
+<<<<<<< Updated upstream
 module.exports = pool;
+=======
+export default pool;
+>>>>>>> Stashed changes
