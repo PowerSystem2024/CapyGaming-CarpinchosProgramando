85ddfdffe262522aa60db679564afc38dc6a73f7
Mariana Aguilera <marian5174@gmail.com>
2025-10-03 00:00:44 -0300

    Agregamos formulario de contacto

---
+++ frontend/src/components/Contacto.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1></h1>
+    <ContactoForm />
+  </div>
+</template>
+
+<script>
+import ContactoForm from "../components/ContactoForm.vue";
+
+export default {
+  components: { ContactoForm }
+};
+</script>
+
+<style scoped>
+div {
+    padding-top: calc(80px + 60px); /* espacio para navbar si tienes */
+}
+</style>
